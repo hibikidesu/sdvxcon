0977f4c233931d64a5a99d061b8cf827fef46b30
hibikidesu <20015683+hibikidesu@users.noreply.github.com>
2020-02-12 13:48:13 +0800

    keyboard input

--- server.py
+++ server.py
@@ -3,10 +3,10 @@ from struct import unpack, calcsize
 import pyvjoy
 from time import sleep
 from enum import IntFlag
+import keyboard
 
-need_config = False
 
-
+BUTTON_LAYOUT = ["a", "s", "d", "f", "g", "h", "j", "k", "l"]
 class Button(IntFlag):
     A = 1
     B = 2
@@ -19,28 +19,16 @@ class Button(IntFlag):
     SERVICE = 256
 
 
-def press_button(con, button: int):
-    con.set_button(button, 1)
-    sleep(0.05)
-    con.set_button(button, 0)
-
-
-def setup_con(con):
-    for x in range(9):
-        print("Pressing button {} in 4s".format(x + 1))
-        sleep(4)
-        press_button(con, x+1)
-    print("Controller Setup")
+def keyboard_input(buttons: int):
+    for i, b in enumerate(Button):
+        if b.value & buttons:
+            keyboard.press_and_release(BUTTON_LAYOUT[i])
 
 
 def run():
     com = Serial("COM2", 57600)
     data = b""
     controller = pyvjoy.VJoyDevice(1)
-    if need_config == True:
-        setup_con(controller)
-        com.close()
-        exit(0)
     while True:
         try:
             incoming_b = com.read()
@@ -51,7 +39,8 @@ def run():
             if len(data) == 9:
                 try:
                     response = unpack(">bii", data)  # buttons, axis l, axis r
-                    controller.data.lButtons = response[0]
+                    #controller.data.lButtons = response[0]
+                    keyboard_input(response[0])
                     controller.data.wAxisXRot = int(response[1] * 32)
                     controller.data.wAxisYRot = int(response[2] * 32)
                     controller.update()
